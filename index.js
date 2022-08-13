90d525f635f2acf8bcc6201693a241cf1d2f29f0
saroj9 <saroj@distronix.in>
2022-08-13 09:18:32 +0530

    bug fixed

--- index.js
+++ index.js
@@ -6,33 +6,31 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json()); //req.body
 
-const calculate = (firstNumber, secondNumber, operator) => {
-  switch (operator) {
-    case "+":
-      return firstNumber + secondNumber;
-    case "-":
-      return firstNumber - secondNumber;
-    case "*":
-      return firstNumber * secondNumber;
-    case "/":
-      return firstNumber / secondNumber;
-    default:
-      return "Invalid operator";
-  }
-};
-
 //routes
 app.get("/api/results", function (req, res) {
   const firstNumber = req.query.first_number;
   const secondNumber = req.query.second_number;
   const operator = req.query.operator;
 
-  const result = calculate(firstNumber, secondNumber, operator);
-
-  res.send({
-    result: result,
-  });
+  if (operator === "addition") {
+    res.status(200).json({
+      result: firstNumber + secondNumber,
+    });
+  } else if (operator === "subtraction") {
+    res.status(200).json({
+      result: firstNumber - secondNumber,
+    });
+  } else if (operator === "multiplication") {
+    res.status(200).json({
+      result: firstNumber * secondNumber,
+    });
+  } else {
+    res.status(400).json({
+      error: "Invalid operator",
+    });
+  }
 });
+
 app.get("/", function (req, res) {
   res.send("Server is running");
 });
